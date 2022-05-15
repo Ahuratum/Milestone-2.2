948e1490cdf14f4af55f474c19112ab225d45e18
Ahuratum <nicolasghagen@gmail.com>
2022-05-15 18:06:14 -0400

    added JS, and updated hobby page and about page

---
+++ README.txt
@@ -0,0 +1,18 @@
+This milestone project will culminate to a portfolio, containing all aspects of web development that I have learned from CST-120
+
+Milestone 1:
+- Created a home page
+- Added an image link of Awareness Technology 
+- Added base information about myself
+- Added an unordered list
+
+Link to video 1: https://drive.google.com/drive/folders/132FNtTtriLHfJwaasMAr4_TvZG1T_4PF?usp=sharing
+
+Milestone 2:
+- Created a hobby page 
+- Added a table with information about 3d printing and links to their wikipedia pages
+- Added iframes of Lex Fridman podcasts
+- Remade the homepage and chaged the background color or each page 
+- Added navigation tabs on the left side of each page
+
+Link to video 2: 
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,44 @@
+// hobby page variables, images
 const game_gallery=document.getElementById('game_gallery');
 const map_pop_up=document.getElementById('map_pop_up');
 const map_item=document.getElementById('map_item');
-const image_vector=[1, 2, 3, 4];
-const image_index=null;
+const hobby_image_vector=[1, 2, 3, 4];
+// hobby page variables, text
 
-// for loop to create new map image elements 
-image_vector.forEach(i=>{
-    const image=document.createElement('img');
+// about page variables, images
+const carousel=document.getElementById('carousel-2');
+// about page variables, text
+
+// ensure document is ready before running
+$('#hobby').ready(function() {
+  //----------------------------------------------
+  // foreach loop for hobby images
+  hobby_image_vector.forEach((i) => {
+    const image = document.createElement("img");
     // use backticks!!! lol
     // had to add two back slashes to make this work
-    image.src=`assets\\img\\gallery\\map-${i}.png`;
-    image.alt='Video Game Map';
-    image.classList.add('game_gallery_image');
+    image.src = `assets\\img\\gallery\\map-${i}.png`;
+    image.alt = "Video Game Map";
+    image.classList.add("game_gallery_image");
     // adding event listener to the new img element
-    image.addEventListener('click',()=>{
-        // the pop_up when image is clicked
-        map_pop_up.hidden = false;
-        map_item.src=`assets\\img\\gallery\\map-${i}.png`;
-    })
+    image.addEventListener("click", () => {
+      // the pop_up when image is clicked
+      map_pop_up.hidden = false;
+      map_item.src = `assets\\img\\gallery\\map-${i}.png`;
+    });
     game_gallery.appendChild(image);
     console.log("image loaded into doc");
+     });
+    //----------------------------------------------
+    //popup close on click
+    map_pop_up.addEventListener("click", () => {
+        map_pop_up.hidden = true;
+        //map_pop_up.style.transform=`translateY(-100%)`;
+        map_pop_up.src = "";
+        map_pop_up.alt = "";
+    });
+    //----------------------------------------------
 })
-map_pop_up.addEventListener('click',()=>{
-    map_pop_up.hidden = true;
-    //map_pop_up.style.transform=`translateY(-100%)`;
-    map_pop_up.src='';
-    map_pop_up.alt='';
-})
+
+
 
